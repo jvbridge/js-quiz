f05892f53f2cb40433c2a24d3c8ab97da77b6043
jvbridge <jvbridge@gmail.com>
2022-03-31 17:36:22 -0700

    made an outline of functionality in js

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// TODO: Set up state zero
+/**
+ * This is an invisible state that will have initializations that will only
+ * happen ONCE when the app loads
+ * adding event listeners and the like
+ */
+
+
+// TODO: Set up state one
+/**
+ * This will need to reset any data from a previous playthrough with the 
+ * exception of the leaderboard
+ * It also notably will set up the elements to be back to this state from state 
+ * three
+ */
+
+// TODO: Set up state two
+/**
+ * This will switch all the elements from state one to state two
+ * It will also set up the first question and start the timer
+ */
+
+
+// TODO: Make a data format for questions
+
+// TODO: setQuestion() function
+
+// TODO: questionCorrect() function
+
+// TODO: questionIncorrect() function
+
+// TODO: nextQuestion() function
+
+
+
+// TODO: Set up state three
+/**
+ * This will switch all the elements from state two to state three. 
+ * It should record the timer's time remaining
+ * It should calculate the current score given what has happened
+ * It should calculate the place the person is in on the leaderboard
+ */
+
+// TODO: addScore() function
+
+// TODO: idea: set the title to be the time remaining
