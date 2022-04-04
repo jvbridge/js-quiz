4a5b33943b44ca1ca66bf1663131c6e3b36b7f1c
jvbridge <jvbridge@gmail.com>
2022-04-03 19:45:44 -0700

    set up state two transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6*/
-// @ts-check
+//  df@ts-check
 
 /*******************************************************************************
  * Global constants
@@ -34,10 +34,16 @@ var questions = [
 var currentState = 0;
 
 /**
+ * The index in the questions[] array that we are currently at. Will be 
+ * @type {number}
+ */
+var currentQuestionIndex = 0
+
+/**
  * 
  * @type {string}
  */
-var currentQuestion = "The Wheel of Time turns, and Ages come and pass, leaving\
+var currentQuestionText = "The Wheel of Time turns, and Ages come and pass, leaving\
  memories that become legend. Legend fades to myth, and even myth is long \
  forgotten when the Age that gave it birth comes again";
 
@@ -62,7 +68,6 @@ var answerC = "The Two Rivers";
   */
 var answerD = "The three-fold land";
 
-var correctAnswer = "C";
 
 /**
  * All the elements that need to be made visible for us to be in state one
@@ -97,12 +102,13 @@ function init(){
     startButtonEle.addEventListener("click", (e) => {
         // Log the event for debugging
         console.log(e);
-        // set our state to state 2 (game state)
-        setStateTwo();
+        // set our state to state 2 (game state) to start the game
+        setState(2);
     });
 
     // push the element into the array
     stateOneEles.push(startButtonEle);
+    stateOneEles.push(document.querySelector("#title"));
 
     // state 2
 
@@ -115,10 +121,27 @@ function init(){
     buttonBEle.addEventListener("click", () => {answer("B")});
     // C answer
     var buttonCEle = document.querySelector("#answer-c");
-    buttonCEle.addEventListener("click", () => {answer("A")});
+    buttonCEle.addEventListener("click", () => {answer("C")});
     // D answer
     var buttonDEle = document.querySelector("#answer-d");
-    buttonDEle.addEventListener("click", () => {answer("A")});
+    buttonDEle.addEventListener("click", () => {answer("D")});
+
+    // push relevant elements onto the array.
+
+    // timer
+    stateTwoEles.push(document.querySelector("#timer-wrapper"))
+
+    // questions/answers
+    stateTwoEles.push(document.querySelector("#question"));
+
+    stateTwoEles.push(buttonAEle);
+    stateTwoEles.push(document.querySelector("#answer-wrapper-a p"));
+    stateTwoEles.push(buttonBEle);
+    stateTwoEles.push(document.querySelector("#answer-wrapper-b p"));
+    stateTwoEles.push(buttonCEle);
+    stateTwoEles.push(document.querySelector("#answer-wrapper-c p"));
+    stateTwoEles.push(buttonDEle);
+    stateTwoEles.push(document.querySelector("#answer-wrapper-d p"));
 
     // state 3
 
@@ -126,14 +149,14 @@ function init(){
     setState(1);
 }
 
-
-
 /**
- * Called when the user clicks on a button to answer a question. 
+ * Called when the user clicks on a button to answer a question. Will resolve if
+ * the user clicked the correct answer
  * @param {String} answerLetter the letter answered with
  */
 function answer(answerLetter){
     console.log("user answered with: ", answerLetter);
+
 }
 
 // TODO: Set up state one
@@ -143,7 +166,48 @@ function answer(answerLetter){
  * @param {number} stateNumber which state we are setting it to (1,2,3)
  */
 function setState(stateNumber){
+    console.log("setting state: " + stateNumber);
+    switch (stateNumber){
+        case 1:
+            setVisible(stateOneEles);
+            setNotVisible(stateTwoEles);
+            setNotVisible(stateThreeEles);
+        break;
+        case 2: 
+            setVisible(stateTwoEles);
+            setNotVisible(stateOneEles);
+            setNotVisible(stateThreeEles);
+        break;
+        case 3: 
+            setVisible(stateThreeEles);
+            setNotVisible(stateOneEles);
+            setNotVisible(stateTwoEles);
+        break;
+        default: throw "current state is not a defined number!";
+    }
 
+
+}
+
+/**
+ * Sets all elements in the array to be visible
+ * @param {Element[]} elements 
+ */
+function setVisible(elements){
+    elements.forEach((element) => {
+        // TODO: check if this actually is wrong
+        element.style.display = "";
+    })
+}
+
+/**
+ * Sets all elements in the array to not be displayed
+ * @param {Element[]} elements 
+ */
+function setNotVisible(elements){
+    elements.forEach((element) => {
+        element.style.display = "none";
+    });
 }
 
 
